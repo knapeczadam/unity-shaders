c7b65936285e627bf9b996d3aef4b252d5cfa0df
Knapecz Ádám <knapecz.adam@gmail.com>
2018-09-19 20:57:25 +0200

    Load models on the fly

--- Assets/Scripts/ModelSceneControl.cs
+++ Assets/Scripts/ModelSceneControl.cs
@@ -13,7 +13,6 @@ namespace UnityStandardAssets.SceneUtils
             Activate
         }
 
-        public DemoModelSystemList demoModels;
         public bool clearOnChange = false;
         public bool mouseWheelZoom = false;
         public Text titleText;
@@ -22,9 +21,10 @@ namespace UnityStandardAssets.SceneUtils
         public Button previousButton;
         public Button nextButton;
 
-
+        private DemoModelSystemList demoModels = new DemoModelSystemList();
         private List<Transform> m_CurrentModelList = new List<Transform>();
         private static int s_SelectedIndex = 0;
+        private static string[] s_GUIDs;
         private static string[] s_Instructions;
         private static int[] s_CamOffsets;
         private Vector3 m_CamOffsetVelocity = Vector3.zero;
@@ -35,6 +35,7 @@ namespace UnityStandardAssets.SceneUtils
         private void Awake()
         {
             LoadInstructions();
+            LoadModels();
             Select(s_SelectedIndex);
 
             previousButton.onClick.AddListener(Previous);
@@ -143,18 +144,35 @@ namespace UnityStandardAssets.SceneUtils
 
         private void LoadInstructions()
         {
-            TextAsset ta = Resources.Load("Instructions") as TextAsset;
+            TextAsset ta = Resources.Load("model_data") as TextAsset;
             string[] lines = ta.text.Split('\n');
             
             s_Instructions = new string[lines.Length];
             s_CamOffsets = new int[lines.Length];
+            s_GUIDs = new string[lines.Length];
             
             for (var i = 0; i < lines.Length; i++)
             {
-                string[] info = lines[i].Split('|');
+                string[] line = lines[i].Split('|');
 
-                s_Instructions[i] = info[0];
-                s_CamOffsets[i] = Int32.Parse(info[1]);
+                s_GUIDs[i] = line[0];
+                s_Instructions[i] = line[1];
+                s_CamOffsets[i] = Int32.Parse(line[2]);
+            }
+        }
+
+        private void LoadModels()
+        {
+            demoModels.items = new DemoModelSystem[s_GUIDs.Length];
+
+            GameObject models = GameObject.FindWithTag("Models");
+            
+            for (int i = 0; i < demoModels.items.Length; i++)
+            {
+                DemoModelSystem container = new DemoModelSystem();
+                container.transform = models.transform.Find(s_GUIDs[i]);
+                container.transform.name = (i + 1).ToString();
+                demoModels.items[i] = container;
             }
         }
 
