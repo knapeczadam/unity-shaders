b0431cd0db68575fb6a6433ec15da7d798830b0a
Knapecz Ádám <knapecz.adam@gmail.com>
2018-09-22 21:27:56 +0200

    Batching

---
+++ Assets/Scripts/BatchCreator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BatchCreator : MonoBehaviour
+{
+    public GameObject staticBatchRoot;
+    
+    private void Start()
+    {
+        StaticBatchingUtility.Combine(staticBatchRoot);
+    }
+}
