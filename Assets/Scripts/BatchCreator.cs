00ff14509624a7e528479d0c4710303b0092d6a7
Knapecz Ádám <knapecz.adam@gmail.com>
2018-09-23 20:15:15 +0200

    Create batch from selected game objects

--- Assets/Scripts/BatchCreator.cs
+++ Assets/Scripts/BatchCreator.cs
@@ -3,9 +3,14 @@
 public class BatchCreator : MonoBehaviour
 {
     public GameObject staticBatchRoot;
+    public GameObject[] gos;
     
     private void Start()
     {
+        if (gos != null)
+        {
+            StaticBatchingUtility.Combine(gos, staticBatchRoot);
+        }
         StaticBatchingUtility.Combine(staticBatchRoot);
     }
 }
