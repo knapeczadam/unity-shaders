a3b680ede8c130f4fc221364ea56c99a547d2bde
Knapecz Ádám <knapecz.adam@gmail.com>
2018-10-17 09:30:42 +0200

    Grayscale on camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,21 +3,26 @@ using UnityStandardAssets.SceneUtils;
 
 public class CameraController : MonoBehaviour
 {
-
-    public bool smoothShift;
-
+    [Header("SHIFTING")]
+    public bool enableSmoothShift;
     public int maxCamOffset;
-
     public int speed;
-    
     private float _timeCounter;
-    
+
+    [Header("REPLACEMENT SHADER")] 
+    public bool enableReplacementShader;
     public Shader replacementShader;
     public string replacementTag;
+    
+    [Header("POST-PROCESSING EFFECTS")]
+    public bool enablePostProcessingEffects;
+    public static bool s_EnablePostProcessingEffects;
+    public Material mat;
+    public static Material s_Mat;
 
     void Update()
     {
-        if (smoothShift)
+        if (enableSmoothShift)
         {
             _timeCounter += Time.deltaTime * speed;
             ModelSceneControl.s_Selected.camOffset = (int) Mathf.PingPong(_timeCounter, maxCamOffset);
@@ -26,18 +31,34 @@ public class CameraController : MonoBehaviour
 
     private void OnEnable()
     {
-        if (replacementShader)
+        if (enableReplacementShader && replacementShader)
         {
             Camera.main.SetReplacementShader(replacementShader, replacementTag);
-            
+        }
+
+        if (enablePostProcessingEffects)
+        {
+            s_Mat = mat;
+            s_EnablePostProcessingEffects = true;
+            RenderTextureController textureController = Camera.main.GetComponent<RenderTextureController>();
+            textureController.enabled = true;
         }
     }
 
     private void OnDisable()
     {
-        if (replacementShader && Camera.main)
+        if (Camera.main)
         {
-            Camera.main.ResetReplacementShader();
+            if (enableReplacementShader)
+            {
+                Camera.main.ResetReplacementShader();
+            }
+    
+            if (enablePostProcessingEffects)
+            {
+                RenderTextureController textureController = Camera.main.GetComponent<RenderTextureController>();
+                textureController.enabled = false;
+            }
         }
     }
 }
---
+++ Assets/Scripts/RenderTextureController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RenderTextureController : MonoBehaviour
+{
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (CameraController.s_EnablePostProcessingEffects && CameraController.s_Mat)
+        {
+            Graphics.Blit(src, dest, CameraController.s_Mat);
+        }
+    }
+}
